441d3625219690642eeaf7a204418dcb1072cf32
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-11-28 18:58:41 +0800

    禁用 Qt 相关
    也许放弃使用 Qt 作为 GUI

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,5 @@
-#include "widget.h"
-#include <QApplication>
-#include <QDate>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    // Widget w;
-    // w.show();
-    QList<QString> test({QString("Hello"), QString("World")});
-    QString str = "Hello, World!";
-    QMap <int, QString> map;
-    QDate date = QDate::currentDate();
-    QVector <int> vector;
-    vector.append(1);
-    vector.append(2);
-    vector.append(3);
-    map.insert(1, "one");
-    map.insert(2, "two");
-    map.insert(3, "three");
-    return a.exec();
+int main(int argc, char *argv[]) {
+    // Test clang-tidy
+    int *leak = new int[100];
+    leak = nullptr;
 }
