84ffca32832094876618be85b890f05a97ae6bce
PilgrimLyieu <PilgrimLyieu@outlook.com>
2024-12-03 17:14:29 +0800

    修复 XMake check 模式中 Windows MinGW64 不支持 Sanitizer 的问题

--- xmake.lua
+++ xmake.lua
@@ -1,4 +1,4 @@
-add_rules("mode.debug", "mode.release", "mode.check")
+add_rules("mode.debug", "mode.release")
 
 set_languages("cxx17")
 set_targetdir("build")
@@ -21,3 +21,27 @@ target("MyToy")
         "include/Places",
         "include/Utilities"
     )
+
+if is_mode("check") then
+    set_symbols("debug")
+    set_optimize("none")
+    add_cxflags(
+        "-fno-strict-aliasing",
+        "-fno-omit-frame-pointer",
+        "-ftrapv",
+        "-fstack-protector-all",
+        "-fstack-check",
+        "-fstack-clash-protection",
+        "-Wall"
+    )
+    if is_os("linux") then
+        -- Not supported for MinGW64 in Windows
+        sanitize = "address"
+        add_cxflags(
+            "-fsanitize=" .. sanitize
+        )
+        add_ldflags(
+            "-fsanitize=" .. sanitize
+        )
+    end
+end
