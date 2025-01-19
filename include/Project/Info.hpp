0481765c1506e15b93b91172b4535fbf4c7f0718
PilgrimLyieu <PilgrimLyieu@outlook.com>
2025-01-19 08:14:54 +0800

    更新项目版本号为 0.1.0-SeaOtter

--- include/Project/Info.hpp
+++ include/Project/Info.hpp
@@ -5,7 +5,7 @@
 
 namespace AVSBInfo {
 const string PROJECT_NAME = "Ants vs. SomeBees";
-const string VERSION = "0.1.0-developing";
+const string VERSION = "0.1.0-SeaOtter";
 const string AUTHOR = "PilgrimLyieu";
 const string EMAIL = "pilgrimlyieu@outlook.com";
 const string DESCRIPTION = "C++ version of the game Ants vs. SomeBees";
